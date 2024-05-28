95e97c039b520aa6c7b28d94253d5154445af71b
bjadczak <73365815+bjadczak@users.noreply.github.com>
2024-05-28 17:43:47 +0200

    Project clean-up

--- src/Scene.h
+++ src/Scene.h
@@ -6,42 +6,23 @@
 #define PUMAROBOT_SCENE_H
 
 
-#include "robot/Robot.h"
 #include "camera/CameraAnchorFree.h"
 #include "app/AppContext.h"
 
 class Scene {
 
     Shader whiteShader;
-    Shader pbrShader;
-    Shader shadowShader;
+    Shader phongShader;
     Shader skyboxShader;
-    Shader trailShader;
     Shader pointShader;
-    Shader flameShader;
-    Shader sparkShader;
 
     AppContext &appContext;
 
-    void drawMirrorScene(PointLight &pointLight);
-    void setupMirrorPhong(PointLight &pointLight);
-    void drawSceneNoMirror(bool drawCylinder = true);
-    void drawSceneOnlyMirrorFront();
-
-    void drawShadowVolume(PointLight &pointLight);
     void setupPhong(PointLight &pointLight);
     void drawScene();
-    void createShadowMask(PointLight &pointLight);
-    void setupShadowedPhong(PointLight &pointLight);
 
-    void drawFlames();
-    void drawFlamesNormal();
-    void drawFlamesMirrored();
     void drawSkybox();
-    void drawTrail();
     void drawPointLight(Point &point);
-    void drawSkyboxMirrored();
-    void drawSparks();
 
 public:
     explicit Scene(AppContext &appContext);
